8295ceabd4e4b0918d68ec9a7947559e81f59f9a
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-06-27 21:23:13 +0530

    def isprime(n):     for i in range(2,int((n**0.5)+1)):         if(n%i==0):             return False     else:         return True n=int(input()) if(isprime(n)):     rev=0     while(n):         d=n%10         rev=rev*10+d         n=n//10     if(isprime(rev)):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,6 +1,6 @@
 def isprime(n):
     for i in range(2,int((n**0.5)+1)):
-        if n%i==0:
+        if(n%i==0):
             return False
     else:
         return True
@@ -12,8 +12,8 @@ if(isprime(n)):
         rev=rev*10+d
         n=n//10
     if(isprime(rev)):
-        print('circular prime')
+        print("circular prime")
     else:
-        print('prime but not a circular prime')
+        print("prime but not a circular prime")
 else:
-    print('not prime')
+    print("not prime")
