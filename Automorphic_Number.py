7e2437892015c35be7d7733c9cddfc06ffc51343
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-06-29 21:42:55 +0530

    n=int(input()) count=1 sq=n*n temp=n while(n!=0):     count=count*10     n=n//10 if(sq%count==temp):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -6,6 +6,6 @@ while(n!=0):
     count=count*10
     n=n//10
 if(sq%count==temp):
-    print('Automorphic Number');
+    print("Automorphic Number")
 else:
-    print('Not an Automorphic Number');
+    print("Not an Automorphic Number")
