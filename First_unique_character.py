48b0c013478cd616a50a86d6a6baf1ef6286c870
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-10-07 20:24:14 +0530

    s=input() b=s.lower() c1=0 for i in range(len(b)):     c=0     for j in range(len(b)):         if(i!=j and b[i]==b[j]):             c+=1     if(c==0):         c1+=1         break if(c1==0):     print("-1") else:     print(b[i])

---
+++ First_unique_character.py
@@ -0,0 +1,15 @@
+s=input()
+b=s.lower()
+c1=0
+for i in range(len(b)):
+    c=0
+    for j in range(len(b)):
+        if(i!=j and b[i]==b[j]):
+            c+=1
+    if(c==0):
+        c1+=1
+        break
+if(c1==0):
+    print("-1")
+else:
+    print(b[i])
