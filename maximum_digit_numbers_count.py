ef80bfc38838626ab7ec51c9c8f88d316f06c78a
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-10-10 15:50:19 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 maxi=0 for i in a:     r=str(i)     if(len(r)>maxi):         maxi=len(r) for i in a:     if len(str(i))==maxi:         print(i,end=' ')

---
+++ maximum_digit_numbers_count.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+maxi=0
+for i in a:
+    r=str(i)
+    if(len(r)>maxi):
+        maxi=len(r)
+for i in a:
+    if len(str(i))==maxi:
+        print(i,end=' ')
