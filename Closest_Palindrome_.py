c4f044f4a74e5e9571f2d1a22f939bb5045eb6a5
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2022-10-11 23:28:26 +0530

    n=int(input()) for i in range(n-1,-1,-1):     temp=i     rev=0     while(temp):         d=temp%10         rev=rev*10+d         temp=temp//10     if(rev==i):         bn=i         a=n-bn         break for i in range(n+1,n+10000):     temp=i     rev=0     while(temp):         d=temp%10         rev=rev*10+d         temp=temp//10     if(rev==i):         an=i         b=an-n         break if(a==b):     print(bn,an) elif(a<b):     print(bn) else:     print(an)

---
+++ Closest_Palindrome_.py
@@ -0,0 +1,30 @@
+n=int(input())
+for i in range(n-1,-1,-1):
+    temp=i
+    rev=0
+    while(temp):
+        d=temp%10
+        rev=rev*10+d
+        temp=temp//10
+    if(rev==i):
+        bn=i
+        a=n-bn
+        break
+for i in range(n+1,n+10000):
+    temp=i
+    rev=0
+    while(temp):
+        d=temp%10
+        rev=rev*10+d
+        temp=temp//10
+    if(rev==i):
+        an=i
+        b=an-n
+        break
+if(a==b):
+    print(bn,an)
+elif(a<b):
+    print(bn)
+else:
+    print(an)
+    
